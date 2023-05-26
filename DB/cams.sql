2b3a57a49aa6e22053a052250f331d12a0dd2047
Thepurplebull <ndadilladismas@gmail.com>
2023-05-26 16:41:24 +0300

    database

--- DB/cams.sql
+++ DB/cams.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 08, 2023 at 12:23 PM
+-- Generation Time: May 26, 2023 at 03:40 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -24,6 +24,52 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `attendance`
+--
+
+CREATE TABLE `attendance` (
+  `attendance_id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `class_id` int(11) NOT NULL,
+  `classteacher_id` int(11) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `attendance`
+--
+
+INSERT INTO `attendance` (`attendance_id`, `student_id`, `class_id`, `classteacher_id`, `date`) VALUES
+(1, 1, 2, 1, '2023-05-26'),
+(2, 4, 2, 1, '2023-05-26'),
+(3, 2, 2, 1, '2023-05-25');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `classes`
+--
+
+CREATE TABLE `classes` (
+  `class_id` int(11) NOT NULL,
+  `name` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `classes`
+--
+
+INSERT INTO `classes` (`class_id`, `name`) VALUES
+(1, 'Darasa la 1'),
+(2, 'Darasa la 2'),
+(3, 'Darasa la 3'),
+(4, 'Darasa la 4'),
+(5, 'Darasa la 6'),
+(6, 'Darasa la 7');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `classteacher`
 --
 
@@ -32,25 +78,161 @@ CREATE TABLE `classteacher` (
   `first_name` text NOT NULL,
   `middle_name` text NOT NULL,
   `last_name` text NOT NULL,
-  `password` varchar(100) NOT NULL
+  `password` varchar(100) NOT NULL,
+  `class_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `classteacher`
 --
 
-INSERT INTO `classteacher` (`classteacher_id`, `first_name`, `middle_name`, `last_name`, `password`) VALUES
-('1', 'Juma', 'Mwamuzi', 'Ekole', '1234567890');
+INSERT INTO `classteacher` (`classteacher_id`, `first_name`, `middle_name`, `last_name`, `password`, `class_id`) VALUES
+('1', 'Juma', 'Mwamuzi', 'Ekole', '1234567890', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `student_id` int(11) NOT NULL,
+  `firstname` text NOT NULL,
+  `secondname` text NOT NULL,
+  `lastname` text NOT NULL,
+  `gender` text NOT NULL,
+  `class_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`student_id`, `firstname`, `secondname`, `lastname`, `gender`, `class_id`) VALUES
+(1, 'Mawe ', 'Makubwa', 'Jiwe', 'M', 2),
+(2, 'Monica', 'Julius', 'Mtakuja', 'F', 2),
+(3, 'Domo', 'Kubwa', 'Manenomengi', 'M', 2),
+(4, 'Lucy', 'Luciana', 'John', 'F', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subjects`
+--
+
+CREATE TABLE `subjects` (
+  `subject_id` int(11) NOT NULL,
+  `subject_name` text NOT NULL,
+  `class_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `subjects`
+--
+
+INSERT INTO `subjects` (`subject_id`, `subject_name`, `class_id`) VALUES
+(1, 'English std 2', 2),
+(2, 'KIswahili std 2', 2),
+(3, 'Haiba Na Michezo std 2', 2),
+(4, 'Sayansi std 2', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `timetable`
+--
+
+CREATE TABLE `timetable` (
+  `timetable_id` int(11) NOT NULL,
+  `day` text NOT NULL,
+  `subject_id` int(11) NOT NULL,
+  `start_time` varchar(255) NOT NULL,
+  `end_time` varchar(255) NOT NULL,
+  `classteacher_id` int(11) NOT NULL,
+  `class_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `timetable`
+--
+
+INSERT INTO `timetable` (`timetable_id`, `day`, `subject_id`, `start_time`, `end_time`, `classteacher_id`, `class_id`) VALUES
+(1, 'Jumatatu', 1, '13:00', '14:00', 1, 2),
+(2, 'Jumatatu', 2, '10:00', '02:00', 1, 2);
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `attendance`
+--
+ALTER TABLE `attendance`
+  ADD PRIMARY KEY (`attendance_id`);
+
+--
+-- Indexes for table `classes`
+--
+ALTER TABLE `classes`
+  ADD PRIMARY KEY (`class_id`);
+
+--
 -- Indexes for table `classteacher`
 --
 ALTER TABLE `classteacher`
   ADD PRIMARY KEY (`classteacher_id`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`student_id`);
+
+--
+-- Indexes for table `subjects`
+--
+ALTER TABLE `subjects`
+  ADD PRIMARY KEY (`subject_id`);
+
+--
+-- Indexes for table `timetable`
+--
+ALTER TABLE `timetable`
+  ADD PRIMARY KEY (`timetable_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `attendance`
+--
+ALTER TABLE `attendance`
+  MODIFY `attendance_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `classes`
+--
+ALTER TABLE `classes`
+  MODIFY `class_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `students`
+--
+ALTER TABLE `students`
+  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `subjects`
+--
+ALTER TABLE `subjects`
+  MODIFY `subject_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `timetable`
+--
+ALTER TABLE `timetable`
+  MODIFY `timetable_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
